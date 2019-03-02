63efc0639d6db5b83ea0ec729c0d33fa49fbc9bf
xuanlang1216 <xuanlang1216@gmail.com>
2019-03-01 23:15:06 -0500

    simple GUI added

---
+++ src/GUI/Gui.scala
@@ -0,0 +1,35 @@
+package GUI
+
+import scalafx.animation.AnimationTimer
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.control.TextField
+import scalafx.scene.layout.GridPane
+import THE_GAME._
+
+object Gui extends JFXApp {
+
+  val game = new Game
+
+  var CardDisplay :TextField = new TextField{
+    editable = false
+    style = "-fx-font: 18 ariel;"
+  }
+
+  val Start_B = new StartButton(game,2 ,2)
+  val Call_B = new CallButton(game,2,2)
+
+  this.stage = new PrimaryStage{
+    title = "The Slapjack Game"
+    scene = new Scene(){
+      content = List(
+         new GridPane{
+           add(Start_B,0,0,3,2)
+           add(CardDisplay,2,2)
+           add(Call_B,0,4)
+        }
+      )
+    }
+  }
+}
---
+++ src/GUI/MyButton.scala
@@ -0,0 +1,25 @@
+package GUI
+
+import java.io.FileNotFoundException
+import java.nio.file.{Files, Paths}
+import scalafx.Includes._
+import scalafx.event.ActionEvent
+import scalafx.scene.control.Button
+import scala.io.Source
+import THE_GAME._
+
+class MyButton (game:Game,xScale:Double,yScale:Double) extends Button {
+  minWidth = 100 * xScale
+  minHeight = 100 * yScale
+  style = "-fx-font: 12 ariel;"
+}
+
+class StartButton(game: Game, xScale: Double = 1.0, yScale: Double = 1.0)extends MyButton(game,xScale,yScale){
+  text = "Start"
+  style = "-fx-font: 24 ariel;"
+}
+
+class CallButton(game: Game, xScale: Double = 1.0, yScale: Double = 1.0)extends MyButton(game,xScale,yScale){
+  text = "Call!"
+  style = "-fx-font: 24 ariel;"
+}
