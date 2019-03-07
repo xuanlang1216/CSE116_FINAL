6db9bb94d84ae9ed7eb8f4884114b95bc016b2c7
xuanlang1216 <xuanlang1216@gmail.com>
2019-03-06 22:51:26 -0500

    testcase

--- src/THE_GAME/Game.scala
+++ src/THE_GAME/Game.scala
@@ -6,13 +6,26 @@ class Game {
 
   var thisCards :DeckOfCards =  new DeckOfCards
 
+  var Player1 : Player = new Player
+  var Player2:Player=new Player
+  var Player3:Player =new Player
+  var Player4:Player=new Player
+
 
   def start():Unit={
     thisCards.Shuffle()
+    thisCards.deal(Player1,Player2,Player3,Player4)
   }
 
   def call():Unit= {
 
+  }
+
+  def Has_A_Winner():Boolean={
+    if (Player1.myCards.isEmpty|Player2.myCards.isEmpty|Player3.myCards.isEmpty|Player4.myCards.isEmpty)
+      true
+    else
+      false
   }
 
 
