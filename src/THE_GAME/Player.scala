32c34daf89900b91cfe7a370b8b4473df64044f1
xuanlang1216 <47255973+xuanlang1216@users.noreply.github.com>
2019-03-30 22:40:57 -0400

    Update Player.scala

--- src/THE_GAME/Player.scala
+++ src/THE_GAME/Player.scala
@@ -2,11 +2,5 @@ package THE_GAME
 
 class Player {
 
-   var myCards : Array[Cards]= new Array[Cards](13)
 
-   override def toString: String = {
-      var ret:String=""
-      myCards.foreach(x=> ret=ret+x.suit+x.Num.toString+"\n")
-     ret
-   }
 }
